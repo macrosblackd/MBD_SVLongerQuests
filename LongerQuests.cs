61c082eae314b3ebc21a19f351bab52944d57977
Justin Drury <macrosblackd@gmail.com>
2025-07-26 03:51:50 -0500

    Add project files.

---
+++ LongerQuests.cs
@@ -0,0 +1,61 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Random = System.Random;
+
+namespace MBD_SVLongerQuests
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class LongerQuests : BaseUnityPlugin
+    {
+        public const string pluginGuid = "macrosblackd.starvalormods.longerquests";
+        public const string pluginName = "Longer Quest Duration";
+        public const string pluginVersion = "0.1.0";
+
+        static ManualLogSource logger = new ManualLogSource("(MBD) LongerQuests");
+        
+        private static ConfigEntry<int> durationMultiplier;
+
+        private void Awake()
+        {
+            BepInEx.Logging.Logger.Sources.Add(logger);
+            LoadConfig();
+            Harmony.CreateAndPatchAll(typeof(LongerQuests));
+            logger.Log(LogLevel.Info, $"Loaded {pluginName} v{pluginVersion}");
+        }
+
+        private void LoadConfig()
+        {
+            durationMultiplier = Config.Bind<int>("General", "QuestDurationMultiplier", 5,
+                "The multiplier for how much longer you want quests to last.");
+        }
+
+        [HarmonyPatch(typeof(SM_MissionBoard), nameof(SM_MissionBoard.GenerateQuests))]
+        [HarmonyPostfix()]
+        public static void PostFix(SM_MissionBoard __instance)
+        {
+            foreach (var stationQuest in __instance.quests)
+            {
+                if (stationQuest.quest.deliveryTime > 0 && stationQuest.quest.par1 != -55)
+                {
+                    logger.LogInfo($"Found quest with deliver time: {stationQuest.quest.deliveryTime}");
+                    logger.LogDebug($"TextPar1: {stationQuest.quest.textPar1}");
+                    logger.LogDebug($"par1: {stationQuest.quest.par1}");
+                    stationQuest.quest.deliveryTime *= durationMultiplier.Value;
+                    stationQuest.quest.par1 = -55;
+                    logger.LogInfo($"New Delivery time: {stationQuest.quest.deliveryTime}");
+
+                }
+            }
+        }
+    }
+}
